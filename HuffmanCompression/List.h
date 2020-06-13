d1b866d21bfa478f6a63e685a99b8dd6f8cd6a07
Hritik <>
2020-06-13 13:37:20 +0530

    List Header Created

---
+++ HuffmanCompression/List.h
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct list{
+    int freq;
+    unsigned char key;
+    struct list *next;
+};
+struct list *current=NULL;
+struct list *head=NULL;
+struct list* valueAt(int n)
+{   struct list *temp=head;
+    int c=0;
+    while(temp!=NULL && c!=n){
+        //printf("%d  :%c\n",temp->freq,temp->key);
+        c++;
+        temp=temp->next;
+    }
+    return(temp);
+}
+
+void display()
+{   struct list *temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d  :%c\n",temp->freq,temp->key);
+        temp=temp->next;
+    }
+    //return(temp);
+}
+
+void createNode(struct list *m,int freq,unsigned char key)
+{
+    m->freq=freq;
+    m->key=key;
+    m->next=NULL;
+}
+
+void Rem(int n)
+{
+struct list *temp=valueAt(n-1);
+struct list *temp2=temp->next->next;
+free(temp->next);
+temp->next=temp2;
+}
+
+void Add(int freq,unsigned char key)
+{
+    if(current==NULL)
+        {
+        current=(struct list *)malloc(sizeof(struct list));
+        createNode(current,freq,key);
+        head=current;
+        }
+    else
+        {
+        struct list *current2=(struct list *)malloc(sizeof(struct list));
+        current->next=current2;
+        createNode(current2,freq,key);
+        current=current2;
+        }
+}
--- HuffmanCompression/main.c
+++ HuffmanCompression/main.c
@@ -1,8 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "List.h"
+
+struct HLeaf{
+    int freq;
+    unsigned char key;
+    struct HLeaf *left;
+    struct HLeaf *right;
+};
+
+
+
+void createRTree();
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("Started\n");
+    Add(45,'t');
+    Add(15,'o');
+    Add(23,'a');
+    Add(78,'z');
+    printf("freq:key\n");
+    //struct list *temp=head;
+    Rem(2);
+    display();
+    //next(temp);
     return 0;
 }
